43bf6dbda9240d09a7fd65928a00f64af5744765
Björn Bergdagl <molnet@gmail.com>
2018-11-19 14:00:53 +0100

    Added sql-testdata

---
+++ src/res/paraplyet.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT * FROM paraplyet.new_table
+LIMIT 0, 1000
+
+-- Date: 2018-11-13 11:07
+*/
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10000','I0019N','VT14');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10001','I0019N','VT15');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10002','I0019N','VT16');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10003','I0019N','VT17');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10004','I0019N','VT18');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10005','I0019N','VT19');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10006','D0004N','VT14');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10007','D0004N','VT15');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10008','D0004N','VT16');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10009','D0004N','VT17');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10010','D0004N','VT18');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10011','D0004N','VT19');
+INSERT INTO `new_table` (`EnrollCode`,`CourseCode`,`SemesterCode`) VALUES ('LTU10012','D0006N','VT14');
